ace15a17ec0ef54f1545d1288fe39dc140b3c102
larlito <aligaziev04@mail.ru>
2024-05-05 13:56:59 +0300

    class PassengerPlane full

--- main.py
+++ main.py
@@ -27,4 +27,32 @@ class Book:
 
 Book1 = Book('Harry Potter','J.K. Rowling',335 )
 print(Book1.open_page(34))
+print(f'Информация о книге:')
+
+#3
+class PassengerPlane:
+    def __init__(self,manufacturer: str, model: str, capacity: int, current_height: float, current_speed: float):
+        self.manufacturer = manufacturer
+        self.model = model
+        self.capacity = capacity
+        self.current_height = current_height
+        self.current_speed = current_speed
+
+    def takeoff_plane(self):
+        return 'Самолет взлетел!'
+
+    def landing_plane(self):
+        return 'Самолет приземлился!'
+
+    def сhange_height(self, new_height: float):
+        plane1.current_height = new_height
+        return f'Новая высота {plane1.current_height}'
+
+
+
+plane1 = PassengerPlane('Pobeda', 's999', 1000,5231.123,700)
+plane1.сhange_height(1)
+print(f'Информация о самолете:\nПроизводитель: {plane1.manufacturer}\nМодель: {plane1.model}\nВместимость: {plane1.capacity}\nТекущая высота: {plane1.current_height}\nТекущая скорость: {plane1.current_speed}')
+
+
 
